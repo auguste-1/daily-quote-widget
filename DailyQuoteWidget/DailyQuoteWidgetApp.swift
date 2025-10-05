7224ccc7ef823ed6cd0aeecac40f8f124d268153
Auguste <auguste.ruliene@gmail.com>
2025-10-05 22:14:28 +0100

    Initial Commit

---
+++ DailyQuoteWidget/DailyQuoteWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyQuoteWidgetApp.swift
+//  DailyQuoteWidget
+//
+//  Created by Augustė Rulienė on 05/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct DailyQuoteWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
